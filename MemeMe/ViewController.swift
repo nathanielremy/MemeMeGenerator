f7fcc78a7db55ddef836e3586867e79e75dd6030
Nathaniel Remy <nathanielremy@Nathaniels-MacBook-Pro.local>
2017-06-01 15:40:53 -0300

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Nathaniel Remy on 2017-06-01.
+//  Copyright © 2017 Nathaniel Remy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
